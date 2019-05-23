d8a2184effce065b3adb02a5dbd12fcc0ee56532
ixtears23 <ixtears23@icloud.com>
2019-05-24 02:02:53 +0900

    .

--- demo-boot-web2/src/main/java/me/junseok/demobootweb/WebConfig.java
+++ demo-boot-web2/src/main/java/me/junseok/demobootweb/WebConfig.java
@@ -1,7 +1,11 @@
 package me.junseok.demobootweb;
 
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -10,7 +14,15 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(new GreetingInterceptor()).order(0);
-		registry.addInterceptor(new AnotherInterceptor()).order(-1)
-				.addPathPatterns("/hi/");
+		registry.addInterceptor(new AnotherInterceptor())
+				.addPathPatterns("/hi/").order(-1);
+	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/mobile/**")
+				.addResourceLocations("classpath:/mobile/")
+				.setCacheControl(CacheControl.maxAge(10, TimeUnit.MINUTES));
+				
 	}
 }
--- demo-boot-web2/src/test/java/me/junseok/demobootweb/SampleControllerTest.java
+++ demo-boot-web2/src/test/java/me/junseok/demobootweb/SampleControllerTest.java
@@ -3,6 +3,7 @@ package me.junseok.demobootweb;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.hamcrest.Matchers;
@@ -11,6 +12,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -44,10 +46,11 @@ public class SampleControllerTest {
 	
 	@Test
 	public void hellostatic() throws Exception {
-		this.mockMvc.perform(get("/index.html"))
+		this.mockMvc.perform(get("/mobile/index.html"))
 				.andDo(print())
 				.andExpect(status().isOk())
-				.andExpect(content().string(Matchers.containsString("hello index")));
+				.andExpect(content().string(Matchers.containsString("hello mobile")))
+				.andExpect(header().exists(HttpHeaders.CACHE_CONTROL));
 		
 	}
 }
